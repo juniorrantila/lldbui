d6383a75f697a1d7603139b4a67a539b23840e9d
Dennis Schön <mail@dennis-schoen.de>
2024-04-19 14:58:10 +0200

    remove spurious seperator in bottom bar

--- src/app/components/bottom_bar.rs
+++ src/app/components/bottom_bar.rs
@@ -1,20 +1,27 @@
-use egui::{
-    warn_if_debug_build, widgets::global_dark_light_mode_switch, Align, Layout, RichText, Ui,
-};
+use egui::{widgets::global_dark_light_mode_switch, Align, Layout, RichText, Ui};
 use lldb::SBDebugger;
 
 use crate::app::App;
 
 pub fn add(app: &mut App, ui: &mut Ui) {
     ui.horizontal(|ui| {
-        warn_if_debug_build(ui);
-        ui.separator();
+        if cfg!(debug_assertions) {
+            ui.label(
+                RichText::new("⚠ Debug build ⚠")
+                    .small()
+                    .color(ui.visuals().warn_fg_color),
+            )
+            .on_hover_text("egui was compiled with debug assertions enabled.");
+            ui.separator();
+        }
+
         ui.label(RichText::new(format!("{:.2} fps", app.frame_history.fps())).small())
             .on_hover_text(format!(
                 "Mean CPU usage: {:.2} ms / frame",
                 1e3 * app.frame_history.mean_frame_time()
             ));
         ui.separator();
+
         ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
             global_dark_light_mode_switch(ui);
             ui.separator();
